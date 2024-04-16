76550782a6802d3d5cb7c83cffd3dbd5c2f9e950
Ian-Flury <ianflury123@gmail.com>
2024-04-16 14:29:06 -0700

    removed old tools that aren't needed.

--- build_container.sh
+++ build_container.sh
@@ -8,7 +8,4 @@ sudo touch "/etc/udev/rules.d/51-usbblater.rules"
 echo "SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6001", MODE="0666"" > /etc/udev/rules.d/51-usbblater.rules
 sudo sudo udevadm control --reload-rules
 
-# Need these tools for mounting a shared directory
-sudo apt-get install cifs-utils
-
 docker build -t quartus18 .
