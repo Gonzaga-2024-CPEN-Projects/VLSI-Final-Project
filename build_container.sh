0817e767d6fa31afd6815b05f549fae9a9eda3f0
Ian-Flury <ianflury123@gmail.com>
2024-04-18 15:06:28 -0700

    suppress mkdir output in the build_container script

--- build_container.sh
+++ build_container.sh
@@ -9,7 +9,7 @@ echo "SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6001", MODE=
 sudo sudo udevadm control --reload-rules
 
 # Create the shared folder for the container
-mkdir $HOME/DSD_Designs
+mkdir -p $HOME/DSD_Designs
 # We need to make sure that this dir is rwx or we won't 
 # be able to do anything with it on our system or the docker.
 sudo chmod 777 $HOME/DSD_Designs
