c8c602b80a5457f8e6318c40027577393bb18971
Ian-Flury <ianflury123@gmail.com>
2024-04-19 12:09:19 -0700

    fixed the way that the usb blaster rules file is created.

--- build_container.sh
+++ build_container.sh
@@ -4,9 +4,9 @@
 
 ## This will pass the usb blaster through to the docker container
 echo "Creating usb-blaster rules file..."
-sudo touch "/etc/udev/rules.d/51-usbblater.rules"
-echo "SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6001", MODE="0666"" > /etc/udev/rules.d/51-usbblater.rules
-sudo sudo udevadm control --reload-rules
+sudo touch /etc/udev/rules.d/51-usbblaster.rules
+sudo mv usb_rules.txt /etc/udev/rules.d/51-usbblaster.rules
+sudo udevadm control --reload-rules
 
 # Create the shared folder for the container
 mkdir -p $HOME/DSD_Designs
