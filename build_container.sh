f29efd03e5bc081244230ba1651170c043b83a7a
Ian-Flury <ianflury123@gmail.com>
2024-04-22 11:48:41 -0700

    added wgets for quartus and ModelSim .run downloads to the build_container.sh

--- build_container.sh
+++ build_container.sh
@@ -1,6 +1,16 @@
 # A script that goes through the steps to build the Digital System Designs
 # Docker Container...
 
+# First Download the setup files necessary for the build
+echo "Downloading Quartus, Cyclone, and ModelSim files..."
+filename='ModelSimSetup-18.1.0.625-linux.run'
+# Don't download the file if it already exists.
+test -f $filename && echo 'ModelSim.run file exists.' || wget -O ModelSimSetup-18.1.0.625-linux.run https://cdrdv2.intel.com/v1/dl/getContent/750368/750371?filename=ModelSimSetup-18.1.0.625-linux.run
+
+filename='QuartusLiteSetup-18.1.0.625-linux.run'
+# Don't download the file if it already exists.
+test -f $filename && echo 'QuartusLiteSetup.run file exists.' || wget -O QuartusLiteSetup-18.1.0.625-linux.run https://cdrdv2.intel.com/v1/dl/getContent/665988/710188?filename=QuartusLiteSetup-18.1.0.625-linux.run
+
 
 ## This will pass the usb blaster through to the docker container
 echo "Creating usb-blaster rules file..."
