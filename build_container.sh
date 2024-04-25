58a7738456f4574825d504d90c8f342135147896
Ian-Flury <ianflury123@gmail.com>
2024-04-25 14:39:25 -0700

    wget eclipse

--- build_container.sh
+++ build_container.sh
@@ -3,6 +3,7 @@
 
 # First Download the setup files necessary for the build
 echo "Downloading Quartus, Cyclone, and ModelSim files..."
+
 filename='ModelSimSetup-18.1.0.625-linux.run'
 # Don't download the file if it already exists.
 test -f $filename && echo 'ModelSim.run file exists.' || wget -O ModelSimSetup-18.1.0.625-linux.run https://cdrdv2.intel.com/v1/dl/getContent/750368/750371?filename=ModelSimSetup-18.1.0.625-linux.run
@@ -11,6 +12,9 @@ filename='QuartusLiteSetup-18.1.0.625-linux.run'
 # Don't download the file if it already exists.
 test -f $filename && echo 'QuartusLiteSetup.run file exists.' || wget -O QuartusLiteSetup-18.1.0.625-linux.run https://cdrdv2.intel.com/v1/dl/getContent/665988/710188?filename=QuartusLiteSetup-18.1.0.625-linux.run
 
+filename=eclipse-mars2.tar.gz
+test -f $filename && echo "eclipse-mars2.tar.gz file exists" || wget http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops4/R-4.2.2-201302041200/eclipse-SDK-4.2.2-linux-gtk-x86_64.tar.gz
+mv download.php?file=%2Feclipse%2Fdownloads%2Fdrops4%2FR-4.2.2-201302041200%2Feclipse-SDK-4.2.2-linux-gtk-x86_64.tar.gz eclipse-mars2.tar.gz
 
 ## This will pass the usb blaster through to the docker container
 echo "Creating usb-blaster rules file..."
