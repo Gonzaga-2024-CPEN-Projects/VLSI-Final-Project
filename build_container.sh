b371fe11d9edf3ef6fd906a95224461c1d561165
Ian-Flury <ianflury123@gmail.com>
2024-04-30 14:01:10 -0700

    removed the wget for eclipse bc its under version control.

--- build_container.sh
+++ build_container.sh
@@ -12,9 +12,6 @@ filename='QuartusLiteSetup-18.1.0.625-linux.run'
 # Don't download the file if it already exists.
 test -f $filename && echo 'QuartusLiteSetup.run file exists.' || wget -O QuartusLiteSetup-18.1.0.625-linux.run https://cdrdv2.intel.com/v1/dl/getContent/665988/710188?filename=QuartusLiteSetup-18.1.0.625-linux.run
 
-filename=eclipse-mars2.tar.gz
-test -f $filename && echo "eclipse-mars2.tar.gz file exists" || wget http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops4/R-4.2.2-201302041200/eclipse-SDK-4.2.2-linux-gtk-x86_64.tar.gz
-mv download.php?file=%2Feclipse%2Fdownloads%2Fdrops4%2FR-4.2.2-201302041200%2Feclipse-SDK-4.2.2-linux-gtk-x86_64.tar.gz eclipse-mars2.tar.gz
 
 ## This will pass the usb blaster through to the docker container
 echo "Creating usb-blaster rules file..."
