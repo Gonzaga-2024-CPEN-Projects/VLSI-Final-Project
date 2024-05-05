6f9790a6b983ff0d76b9d9728f5a78f5a808ec54
Ian-Flury <ianflury123@gmail.com>
2024-05-05 15:53:26 -0700

    changed the original build script to 'build_container_from_scratch.sh' and created a new build script that creates the usb blaster file and the shared folder, and then just pulls the docker image from the docker hub.

---
+++ build_container_quick.sh
@@ -0,0 +1,18 @@
+# This script pulls the EDA tools docker image from the docker hub
+# without building from scratch. It also creates a usbblaster rules
+# files that is needed for usb pass-through to the docker container.
+
+## This will pass the usb blaster through to the docker container
+echo "Creating usb-blaster rules file..."
+# sudo touch /etc/udev/rules.d/51-usbblaster.rules
+sudo cp usb_rules.txt /etc/udev/rules.d/51-usbblaster.rules
+sudo udevadm control --reload-rules
+
+# Create the shared folder for the container
+mkdir -p $HOME/DSD_Designs
+# We need to make sure that this dir is rwx or we won't 
+# be able to do anything with it on our system or the docker.
+sudo chmod 777 $HOME/DSD_Designs
+
+echo "pulling the image from docker"
+docker pull "iflury/quartus18:version1.0"
