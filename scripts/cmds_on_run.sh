bb384af2cdc783ecd922224095ee69e768bd10d7
Ian-Flury <ianflury123@gmail.com>
2024-05-05 15:47:22 -0700

    was able to get modelsim to work. think everything works.

--- scripts/cmds_on_run.sh
+++ scripts/cmds_on_run.sh
@@ -1,3 +1,3 @@
 
 cd "/home/boris/DSD_Designs" && xterm &
-xterm -e "/home/boris/altera_lite/quartus/bin/quartus --64bit" & \
+xterm -e "/home/boris/altera_lite/quartus/bin/quartus --64bit" 
--- start_docker.sh
+++ start_docker.sh
@@ -1,3 +1,3 @@
 docker run --rm -v /sys:/sys:ro -v $HOME/DSD_Designs:/home/boris/DSD_Designs \
                 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -e "QT_X11_NO_MITSHM=1" \
-                --privileged -v /dev/bus/usb:/dev/bus/usb quartus18 
+                --privileged -v /dev/bus/usb:/dev/bus/usb iflury/quartus18:version1.0
